68e9629c6fec5a3bc8a0d9857571231547beb9f7
AK47 <147317580+Anish-Kanrar@users.noreply.github.com>
2024-09-24 23:54:28 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 
 function App() {
   var [a,b]=useState(69);
+  
   return (
     <div className='w-full h-screen bg-zinc-900 text-white p-5'>
       <h1>{a}</h1>
